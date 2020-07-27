039ca602ed4264df7d25f4fd32020b92c771c104
rokydas <droky136@gmail.com>
2020-07-27 16:09:24 +0600

    try functionally added

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ generateButton.addEventListener('click', function () {
     realPin.value = randomNumber;
 })
 
+function takeInputPinValue() {
+    let inputPin = document.getElementById('input-pin').value;
+    let inputPinValue;
+    if (inputPin == '') {
+        inputPinValue = 0;
+    }
+    else {
+        inputPinValue = parseInt(inputPin);
+    }
+    return inputPinValue;
+}
+
 function attachDigit(numberId) {
     const numberButton1 = document.getElementById(numberId);
     numberButton1.addEventListener('click', function () {
         let digit = parseInt(numberButton1.innerText);
-        let inputPin = document.getElementById('input-pin').value;
-        let inputPinValue;
-        if (inputPin == '') {
-            inputPinValue = 0;
-        }
-        else {
-            inputPinValue = parseInt(inputPin);
-        }
+        let inputPinValue = takeInputPinValue();
         inputPinValue = (inputPinValue * 10) + digit;
         document.getElementById('input-pin').value = inputPinValue;
     })
@@ -41,44 +46,45 @@ attachDigit('number-button-0');
 const backspaceButton = document.getElementById('backspace-button');
 backspaceButton.addEventListener('click', function () {
     let inputPin = document.getElementById('input-pin').value;
-    let inputPinValue;
-    if (inputPin == '') {
-        inputPinValue = 0;
-    }
-    else {
-        inputPinValue = parseInt(inputPin);
-    }
+    let inputPinValue = takeInputPinValue();
     inputPinValue = parseInt(inputPinValue / 10);
-    if(inputPinValue == 0){
+    if (inputPinValue == 0) {
         document.getElementById('input-pin').value = '';
     }
-    else{
+    else {
         document.getElementById('input-pin').value = inputPinValue;
     }
 })
 
 const acButton = document.getElementById('ac-button');
-acButton.addEventListener('click', function(){
+acButton.addEventListener('click', function () {
     document.getElementById('input-pin').value = '';
 })
 
+let tryCount = 3;
+
 const submitButton = document.getElementById('submit-button');
-submitButton.addEventListener('click', function(){
-    let randomNumber = parseInt(document.getElementById('real-pin').value);
-    let inputPin = document.getElementById('input-pin').value;
-    let inputPinValue;
-    if (inputPin == '') {
-        inputPinValue = 0;
-    }
-    else {
-        inputPinValue = parseInt(inputPin);
-    }
-    if(randomNumber == inputPinValue){
-        document.getElementById('correct-pin').style.display = 'block';
-        document.getElementById('wrong-pin').style.display = 'none';
+submitButton.addEventListener('click', function () {
+    if (tryCount > 0) {
+        let randomNumber = parseInt(document.getElementById('real-pin').value);
+        let inputPin = document.getElementById('input-pin').value;
+        let inputPinValue = takeInputPinValue();
+        if (randomNumber == inputPinValue) {
+            document.getElementById('correct-pin').style.display = 'block';
+            document.getElementById('wrong-pin').style.display = 'none';
+        }
+        else {
+            document.getElementById('wrong-pin').style.display = 'block';
+            document.getElementById('correct-pin').style.display = 'none';
+            tryCount--;
+            document.getElementById('try-text').innerText = tryCount + " try left";
+        }
     }
     else{
-        document.getElementById('wrong-pin').style.display = 'block';
+        document.getElementById('try-full').style.display = 'block';
+        document.getElementById('wrong-pin').style.display = 'none';
         document.getElementById('correct-pin').style.display = 'none';
     }
 })
+
+
